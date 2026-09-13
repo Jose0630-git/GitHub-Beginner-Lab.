a314887f7fa5aec9b606f03c52dbcd53c18df8ef
Jose0630-git <josegithub0630@gmail.com>
2026-09-13 22:06:41 +0800

    Add StudentProfile Visual Studio project.

---
+++ .vs/StudentProfile/StudentProfile/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace StudentProfile
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnEnter = new Button();
+            SuspendLayout();
+            // 
+            // btnEnter
+            // 
+            btnEnter.Location = new Point(212, 174);
+            btnEnter.Name = "btnEnter";
+            btnEnter.Size = new Size(262, 121);
+            btnEnter.TabIndex = 0;
+            btnEnter.Text = "Enter";
+            btnEnter.UseVisualStyleBackColor = true;
+            btnEnter.Click += btnEnter_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnEnter);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnEnter;
+    }
+}
